0fa4b54955d3d92f552f98617c6105c489105eff
Ashish Singh <ashishsinghrana39@gmail.com>
2025-01-18 18:57:12 +0530

    committed

---
+++ Even_Odd.java
@@ -0,0 +1,14 @@
+public class Even_Odd{
+	public static void main(String[] args ){
+		int n=10;
+		for(int i=1;i<=10;i++){
+			if(i%2==0){
+				System.out.println("Even "+i);
+			}
+			else{
+				System.out.println("Odd "+ i);
+			}
+		}
+	}
+}
+
